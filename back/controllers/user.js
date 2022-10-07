a7f9c5260eaa6710104166cc5eb5fb611df7c8d8
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-10-07 18:06:43 +0200

    refactor(controllers): translate in english messages (error/success)
    translate in english remaining french messages for errors and 200/201 status results

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -18,7 +18,7 @@ exports.signup = async (req, res, next) => {
     if(hash){
         try{
             await save(req.body.email,hash)
-            res.status(201).json( {message: "Nouvel utilisateur ajouté"} )
+            res.status(201).json( {message: "User successfully created."} )
         }catch(error){
             res.status(400).json({message: error.message})
         }
@@ -26,14 +26,25 @@ exports.signup = async (req, res, next) => {
 };
 
 exports.login = async (req, res, next) => {
+
     try {    
+
         const user = await findUser(req.body.email);
+
         if(user === null){
-            res.status(401).json( {message: "Paire identifiant/mot de passe incorrecte"} );
+            res.status(401).json( {message: "Invalid email address/password combination"} );
         } else {
-            
-            if(! await compareHashedPasswords(req.body.password, user.password)) {
-                res.status(401).json( {message: "Paire identifiant/mot de passe incorrecte"} )
+
+            let valid 
+
+            try {
+                valid = await compareHashedPasswords(req.body.password, user.password);
+            } catch (error) {
+                res.status(500).json( {message : error.message} );
+                
+            }
+            if(!valid) {
+                res.status(401).json( {message: "Invalid email address/password combination"} )
             } else {
                 res.status(200).json(sessionIds(user._id))
             }
--- back/helper/user.helper.js
+++ back/helper/user.helper.js
@@ -1,11 +1,8 @@
 const bcrypt = require('bcrypt');
 
 const hashPassword = async (password) => bcrypt.hash(password, 10);
-const compareHashedPasswords = async (reqPassword, userPassword) => bcrypt.compare(reqPassword, userPassword, (error) => {
-    if (error) {
-        throw new Error({message : error}); 
-    }
-});
+const compareHashedPasswords = async (reqPassword, userPassword) => bcrypt.compare(reqPassword, userPassword)
+
 
 
 
